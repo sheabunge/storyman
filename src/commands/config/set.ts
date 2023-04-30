d7c3f2e226e830fa865e0f396aeb59819dc79bf5
Shea Bunge <shea@sheabunge.com>
2023-04-30 14:36:25 +1000

    Parse config property values in UserConfig class instead of in Config Set command.

--- src/commands/config/set.ts
+++ src/commands/config/set.ts
@@ -1,13 +1,5 @@
 import { Args } from '@oclif/core'
 import { BaseCommand } from '../../base'
-import { UserConfigProps } from '../../types/userConfigProps'
-
-const parseBoolean = (value: unknown) => !(
-  !value ||
-  typeof value === 'string' && ('0' === value || 'false' === value.toLowerCase() || 'no' === value.toLowerCase())
-)
-
-const booleanProperties: Array<keyof UserConfigProps> = ['requireAuthor', 'requireStory']
 
 export default class ConfigSet extends BaseCommand<typeof ConfigSet> {
   static description = 'Set a new value for a configuration property.'
@@ -35,9 +27,7 @@ export default class ConfigSet extends BaseCommand<typeof ConfigSet> {
       this.error(`invalid property ${prop}`)
     }
 
-    const actualValue = booleanProperties.includes(prop) ? parseBoolean(value) : value
-
-    this.userConfig.set(prop, actualValue)
+    const actualValue = this.userConfig.set(prop, value)
 
     return this.userConfig.write().then(() => {
       this.log(`${prop} = ${actualValue}`)
