49adc8792cc64a952f24574c8ce8ab4437e96705
Shea Bunge <shea.bunge@insigniafinancial.com.au>
2023-05-01 16:29:20 +1000

    Add test for setting a parent and child story.

--- test/commands/set.test.ts
+++ test/commands/set.test.ts
@@ -3,14 +3,24 @@ import { createStoryFile, readStoryFile } from '../utils'
 
 const OLD_STORY = 'TEST-1'
 const NEW_STORY = 'TEST-2'
+const NEW_CHILD_STORY = 'TEST-12'
 
-describe('set', () => {
+describe('set story with child story', () => {
   test
-    .do(async () => await createStoryFile(OLD_STORY))
+    .do(async () => createStoryFile(OLD_STORY))
     .stdout()
     .command(['set', NEW_STORY])
-    .it('runs story set', async context => {
+    .it('sets a new story', async context => {
       expect(context.stdout).to.eq(`Current story is now ${NEW_STORY}.\n`)
       expect(await readStoryFile()).to.eq(NEW_STORY)
+    })
+
+  test
+    .do(async () => createStoryFile(OLD_STORY))
+    .stdout()
+    .command(['set', NEW_STORY, NEW_CHILD_STORY])
+    .it('sets a new story with child story', async context => {
+      expect(context.stdout).to.eq(`Current story is now ${NEW_STORY} ${NEW_CHILD_STORY}.\n`)
+      expect(await readStoryFile()).to.eq(`${NEW_STORY} ${NEW_CHILD_STORY}`)
     })
 })
