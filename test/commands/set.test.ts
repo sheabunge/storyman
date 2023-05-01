75a85c7628a1741e93eed878c91dcead9d4675f0
Shea Bunge <shea@sheabunge.com>
2023-05-01 16:10:29 +1000

    Add proper filesystem bootstraping to set test.

--- test/commands/set.test.ts
+++ test/commands/set.test.ts
@@ -1,12 +1,22 @@
 import { expect, test } from '@oclif/test'
+import { mkdtemp, readFile, writeFile } from 'fs/promises'
+import { chdir } from 'process'
+import { tmpdir } from 'os'
+import { join } from 'path'
 
-const TEST_STORY = 'TEST-2'
+const OLD_STORY = 'TEST-1'
+const NEW_STORY = 'TEST-2'
 
 describe('set', () => {
   test
+    .do(async () => {
+      chdir(await mkdtemp(join(tmpdir(), 'set.test')))
+      await writeFile('.story', OLD_STORY)
+    })
     .stdout()
-    .command(['set', TEST_STORY])
-    .it('runs story set', context => {
-      expect(context.stdout).to.contain(`Current story is now ${TEST_STORY}.\n`)
+    .command(['set', NEW_STORY])
+    .it('runs story set', async context => {
+      expect(context.stdout).to.eq(`Current story is now ${NEW_STORY}.\n`)
+      expect((await readFile('.story')).toString()).to.eq(NEW_STORY)
     })
 })
