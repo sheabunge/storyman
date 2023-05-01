09e8591d900835900e11640763b7061ca984b582
Shea Bunge <shea@sheabunge.com>
2023-05-01 16:25:12 +1000

    Create test utilities file.

--- test/commands/set.test.ts
+++ test/commands/set.test.ts
@@ -1,22 +1,16 @@
 import { expect, test } from '@oclif/test'
-import { mkdtemp, readFile, writeFile } from 'fs/promises'
-import { chdir } from 'process'
-import { tmpdir } from 'os'
-import { join } from 'path'
+import { createStoryFile, readStoryFile } from '../utils'
 
 const OLD_STORY = 'TEST-1'
 const NEW_STORY = 'TEST-2'
 
 describe('set', () => {
   test
-    .do(async () => {
-      chdir(await mkdtemp(join(tmpdir(), 'set.test')))
-      await writeFile('.story', OLD_STORY)
-    })
+    .do(async () => await createStoryFile(OLD_STORY))
     .stdout()
     .command(['set', NEW_STORY])
     .it('runs story set', async context => {
       expect(context.stdout).to.eq(`Current story is now ${NEW_STORY}.\n`)
-      expect((await readFile('.story')).toString()).to.eq(NEW_STORY)
+      expect(await readStoryFile()).to.eq(NEW_STORY)
     })
 })
