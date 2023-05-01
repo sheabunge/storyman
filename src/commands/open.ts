082317994b97a0b078534f6512a26199654ffdf5
Shea Bunge <shea@sheabunge.com>
2023-05-01 17:07:53 +1000

    Prompt the user to enter their Jira URL if not set in config file.

--- src/commands/open.ts
+++ src/commands/open.ts
@@ -1,3 +1,4 @@
+import { ux } from '@oclif/core'
 import { BaseCommand } from '../base'
 import open from 'open'
 import { trimTrailingSlash } from '../utils'
@@ -11,16 +12,31 @@ export default class Open extends BaseCommand<typeof Open> {
 
   static aliases = ['jira']
 
-  async run() {
-    const story = await this.getStory()
-    const baseUrl = await (await this.userConfig).get('jiraUrl')
+  private getBaseUrl = async () => {
+    let url = await (await this.userConfig).get('jiraUrl')
 
-    if (!baseUrl) {
-      this.error('Please set the jiraUrl config property to use this command.')
-      return
+    while (!url) {
+      url = await ux.prompt('What is your Jira site URL?')
+
+      if (!url.toLowerCase().startsWith('http')) {
+        this.warn("That doesn't appear to be a valid URL.")
+        url = ''
+      } else {
+        const userConfig = await this.userConfig
+        userConfig.set('jiraUrl', url)
+        await userConfig.write()
+      }
     }
 
-    const url = `${trimTrailingSlash(baseUrl)}/browse/${story}`
+    return url
+  }
+
+  async run() {
+    const story = await this.getStory()
+    const baseUrl = await this.getBaseUrl()
+
+    const url = `${trimTrailingSlash(baseUrl)}/browse/${story.child ?? story.parent}`
+
     this.log(`Opening ${url}`)
     await open(url)
   }
