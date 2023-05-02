d8ca9a75dd48bde3f6d59dfc293540f27bc35705
Shea Bunge <shea@sheabunge.com>
2023-05-02 14:58:59 +1000

    Revert index file contents.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import { run as start } from '@oclif/core'
-import * as Interfaces from '@oclif/core/lib/interfaces'
-
-export const run = (argv?: string[], options?: Interfaces.LoadOptions): Promise<unknown> => {
-  console.log('argv', argv)
-  return start(argv, options)
-}
+export { run } from '@oclif/core'
