2d77aa084f98f2e9cd9865314d21d7e917077fb6
Shea Bunge <shea@sheabunge.com>
2023-05-09 22:10:50 +1000

    Add tests for clear story command.

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -1,5 +1,5 @@
 import { BaseCommand } from '../base'
-import { Flags } from '@oclif/core'
+import { formatStory } from '../utils'
 
 export default class Clear extends BaseCommand<typeof Clear> {
   static description = 'Reset the current story identifier.'
@@ -11,12 +11,14 @@ export default class Clear extends BaseCommand<typeof Clear> {
   static aliases = ['unset', 'reset']
 
   async run() {
+    const currentStory = await this.getStory()
+
     await this.clearStory()
       .then(() => this.getStory())
       .then(story =>
         story ?
           this.error(`Could not clear story file. Current story is ${story}.`) :
-          this.log('Current story has been cleared.')
+          this.log(`Current story has been cleared, was ${currentStory ? formatStory(currentStory) : 'not set'}.`)
       )
   }
 }
